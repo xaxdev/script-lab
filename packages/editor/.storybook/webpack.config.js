92096efb3ba49af7b9f2083ccff6ebd976927880
Nico Bellante <nicobell@microsoft.com>
2018-11-08 17:00:49 -0800

    still broken, but maybe a little less?

--- packages/editor/.storybook/webpack.config.js
+++ packages/editor/.storybook/webpack.config.js
@@ -1,15 +1,15 @@
-// load the default config generator.
-const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js')
+module.exports = {
+  module: {
+    rules: [{ test: /\.(ts|tsx)$/, loader: require.resolve('ts-loader') }],
+  },
+  // const config = genDefaultConfig(baseConfig, env)
+  // // Extend it as you need.
+  // // For example, add typescript loader:
+  // config.module.rules.push({
+  //   test: /\.(ts|tsx)$/,
+  //   loader: require.resolve('ts-loader'),
+  // })
 
-module.exports = (baseConfig, env) => {
-  const config = genDefaultConfig(baseConfig, env)
-  // Extend it as you need.
-  // For example, add typescript loader:
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    loader: require.resolve('ts-loader'),
-  })
-
-  config.resolve.extensions.push('.ts', '.tsx')
-  return config
+  // config.resolve.extensions.push('.ts', '.tsx')
+  // return config
 }
